d2bd7fbbf7affdc01dc407e702da3a4aef8e66ae
Kristin Brandenburg <kristinburg@gmail.com>
2017-04-04 12:39:37 +0200

    cleaned up data set, removed Germany '1990' error

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   get 'users/dashboard'
-
   get 'users/create'
 
   get 'feedbacks/create'
-
   get 'feedbacks/new'
 
   get 'queries/create'
-
   get 'queries/show'
-
   get 'queries/new'
 
   root to: 'pages#home'
